570fc0fff0a652559067f2df2f252d4fdc1c873a
Le Manh HAI <lemanhhai@Les-Mac-mini.local>
2021-03-30 08:41:04 +0700

    “ini”

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php $data["title"] = "Trang chủ"; ?>
+<?php $is_home = true; ?>
+<?php require "template-parts/layouts/header.php"; ?>
+
+<?php require "template-parts/layouts/modal.php"; ?>
+
+<?php require "template-parts/layouts/footer.php"; ?>
---
+++ template-parts/layouts/modal.php
@@ -0,0 +1,11 @@
+<!-- This is the modal with the outside close button -->
+<div id="modal-close-outside-popup1" uk-modal>
+    <div class="uk-modal-dialog uk-height-large uk-background-norepeat uk-background-cover uk-background-center-center" data-src="images/bg.png" uk-img>
+        <button class="uk-modal-close-outside" type="button" uk-close></button>
+
+    </div>
+</div>
+
+<script>
+    UIkit.modal('#modal-close-outside-popup1').show();
+</script>
